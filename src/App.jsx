a2812e93844717683e256183b6d1b0bed4d12cb5
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-09-24 00:37:12 +0300

    header-hero-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
-{/*import HeroSection from './components/HeroSection';
-import Categories from './components/Categories';
+import HeroSection from './components/HeroSection';
+{/*import Categories from './components/Categories';
 import Campaigns from './components/Campaigns';
 import Favorites from './components/Favorites';
 import MobileApp from './components/MobileApp';
@@ -11,8 +11,8 @@ function App() {
   return (
     <>
       <Header/>
-      {/*<HeroSection/>
-      <Categories/>
+      <HeroSection/>
+      {/*<Categories/>
       <Campaigns/>
       <Favorites/>
       <MobileApp/>
