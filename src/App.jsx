45106b0f263bd3e5bc38df9796aa1582559720b1
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-09-23 23:30:20 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header';
+{/*import HeroSection from './components/HeroSection';
+import Categories from './components/Categories';
+import Campaigns from './components/Campaigns';
+import Favorites from './components/Favorites';
+import MobileApp from './components/MobileApp';
+import Footer from './components/Footer'; */}
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      {/*<HeroSection/>
+      <Categories/>
+      <Campaigns/>
+      <Favorites/>
+      <MobileApp/>
+      <Footer/>*/}
+    </>
+    )
+}
+
+export default App
