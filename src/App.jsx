c249f054d88ba34552fe54f71289072c8fc103b2
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-09-24 15:03:44 +0300

    categories-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
-{/*import Categories from './components/Categories';
-import Campaigns from './components/Campaigns';
+import Categories from './components/Categories';
+{/*import Campaigns from './components/Campaigns';
 import Favorites from './components/Favorites';
 import MobileApp from './components/MobileApp';
 import Footer from './components/Footer'; */}
@@ -12,8 +12,8 @@ function App() {
     <>
       <Header/>
       <HeroSection/>
-      {/*<Categories/>
-      <Campaigns/>
+      <Categories/>
+       {/*<Campaigns/>
       <Favorites/>
       <MobileApp/>
       <Footer/>*/}
---
+++ src/components/Categories.jsx
@@ -0,0 +1,87 @@
+
+function Categories() {
+
+	return (
+		<div className="bg-white py-6">
+			<div className="container mx-auto">
+				<h3 className="font-semibold text-sm mb-2">Kategoriler</h3>
+				<div className="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-11 2xl:grid-cols-12">
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/5697c78dc181490f00c99fea_f7ef7ccb-f3a4-4388-b787-232967c16320.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Yeni Ürünler</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/5fd8c58f3bdc2389a56e0fb0_3322c10f-2eed-4ce9-ab5a-90db5ce067f2.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">İndirimler</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src= "http://cdn.getir.com/cat/551430043427d5010a3a5c5e_1619242669958_1619242670038.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Su & İçecek</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/5928113e616cab00041ec6de_1619242870968_1619242871055.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Meyve & Sebze</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src= "http://cdn.getir.com/cat/566eeb85f9facb0f00b1cb16_1619242817768_1619242817849.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Fırından</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/56dfcfba86004203000a870d_1619242834654_1619242834734.jpeg"className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Temel Gıda</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/56dfe03cf82055030022cdc0_1619242841966_1619242842053.jpeg"className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Atıştırmalık</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/55bca8484dcda90c00e3aa80_1619242741382_1619242741482.jpeg"className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Dondurma</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/56dfed05ab747f03008d9379_1619242850313_1619242850394.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Süt & Kahvaltı</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src= "http://cdn.getir.com/cat/57e2996551f3ee030027e28b_1619242858559_1619242858642.jpeg"className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Fit & Form</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/551430043427d5010a3a5c5c_1619242651249_1619242651335.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Kişisel Bakım</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/55449fdf02632e11003c2da8_1619242719523_1619242719602.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Ev Bakım</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/5b06b056b883b700044e3e4c_1619242916956_1619242917048.jpeg"className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Ev & Yaşam</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src= "http://cdn.getir.com/cat/551430043427d5010a3a5c62_1619242697597_1619242697702.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Teknoloji</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src= "http://cdn.getir.com/cat/551430043427d5010a3a5c63_1619242711604_1619242711687.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Evcil Hayvan</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/551430043427d5010a3a5c5b_1619242620186_1619242620271.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Bebek</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/5697c78dc181490f00c99fea_f7ef7ccb-f3a4-4388-b787-232967c16320.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Cinsel Sağlık</span>
+				</a>
+				<a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
+					<img src="http://cdn.getir.com/cat/551430043427d5010a3a5c5f_1619242679723_1619242679822.jpeg" className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+					<span className="font-semibold transition-colors group-hover:text-purple-700 whitespace-nowrap block mt-2 text-sm">Giyim</span>
+				</a>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Categories
